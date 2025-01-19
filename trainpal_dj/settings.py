15994ae8be00c82a3047f34a6900796efe91a644
Trần Tuấn Anh <anhtran338@gmail.com>
2025-01-19 21:00:16 +0700

    change to redis server

--- trainpal_dj/settings.py
+++ trainpal_dj/settings.py
@@ -53,11 +53,11 @@ ASGI_APPLICATION = 'trainpal_dj.asgi.application'
 
 CHANNEL_LAYERS = {
     'default': {
-        # 'BACKEND': 'channels_redis.core.RedisChannelLayer',
-        'BACKEND': 'channels.layers.InMemoryChannelLayer',
-        # 'CONFIG': {
-        #     "hosts": [('127.0.0.1', 6379)],
-        # },
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        # 'BACKEND': 'channels.layers.InMemoryChannelLayer',
+        'CONFIG': {
+            "hosts": [('127.0.0.1', 6379)],
+        },
     },
 }
 
